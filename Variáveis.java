b5ba92bff82a278392447284eb308c178e5ac20e
Lívia D'Alexandri Cunha <liviadlxcunha@gmail.com>
2025-04-15 22:06:42 -0300

    Add files via upload

---
+++ Variáveis.java
@@ -0,0 +1,16 @@
+package aula02;
+
+public class Variáveis {
+
+	public static void main(String[] args) {
+		
+		int numero_01 = 50;
+		int numero_02 = 25;
+		
+		System.out.println("Variável numero 1 é igual a: " + numero_01);
+		System.out.println("Variável numero 2 é igual a: " +numero_02);
+		
+
+	}
+
+}
